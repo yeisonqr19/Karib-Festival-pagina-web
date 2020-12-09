70f7cc3103b0e1b42d100f41c3222a1ceb34be4d
yeisonqr19 <ing_yeisonqr19@hotmail.com>
2020-12-08 20:14:37 -0500

    finalizada la seccion que hacemos en el main

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 //Variables:
 const iconoMenu = document.getElementById("icono");
 const enlaces = document.getElementById("enlaces");
+let ubicacionPrincipal = window.pageYOffset;
 
 //EventListener:
 
 eventListener();
 function eventListener() {
   iconoMenu.addEventListener("click", menuDesplegable);
+  window.addEventListener("scroll", desplazamientoMenu);
+  enlaces.addEventListener("click", controlMenu);
 }
 //Funciones:
 function menuDesplegable(evt) {
@@ -19,4 +22,19 @@ function menuDesplegable(evt) {
     enlaces.classList.remove("dos");
     enlaces.classList.add("uno");
   }
+}
+
+function desplazamientoMenu() {
+  let desplazamiento = window.pageYOffset;
+  if (ubicacionPrincipal >= desplazamiento) {
+    document.getElementById("navBar").style.top = "0";
+  } else {
+    document.getElementById("navBar").style.top = "-200px";
+  }
+  ubicacionPrincipal = desplazamiento;
+}
+
+function controlMenu() {
+  enlaces.classList.remove("dos");
+  enlaces.classList.add("uno");
 }
